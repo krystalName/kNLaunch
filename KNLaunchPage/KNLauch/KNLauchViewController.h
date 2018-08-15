7cd85aa7b970e0060ac1877f17d969d2c2bec6c4
Leessang <992275294@qq.com>
2018-08-15 15:40:12 +0800

    修改代码

--- KNLaunchPage/KNLauch/KNLauchViewController.h
+++ KNLaunchPage/KNLauch/KNLauchViewController.h
@@ -8,6 +8,38 @@
 
 #import <UIKit/UIKit.h>
 
+///
+typedef void(^ImageLoadingOver)(UIImage *,NSString *);
+
+typedef void(^ClickLauchImage)(UIViewController *);
+
+typedef void(^TheAdEnds)(void);
+
 @interface KNLauchViewController : UIViewController
+
+
+///图片加载完成的回调
+@property(nonatomic, strong)ImageLoadingOver imageloadingBlock;
+
+///图片点击
+@property(nonatomic, strong)ClickLauchImage imageClickBlock;
+
+///时间结束
+@property(nonatomic, strong)TheAdEnds timerOverBlock;
+
+
+
+/**
+ KNLauchViewController
+
+ @param imageFrame imageFrame
+ @param imageURL 图片URL
+ @param timeSecond 显示时间
+ @param hideSkip 时候显示跳过的按钮
+ @param imageloadingBlock 广告图片加载完成
+ @param ClickLauch 点击广告
+ @param TimerOverBlock 时间结束
+ */
++(void)showWithFrame:(CGRect )imageFrame ImageURL:(NSString *)imageURL timeSecond:(NSInteger)timeSecond hideSkip:(BOOL)hideSkip ImageLoadingOver:(ImageLoadingOver)imageloadingBlock clickLauchImage:(ClickLauchImage)ClickLauch TimerOver:(TheAdEnds)TimerOverBlock;
 
 @end
