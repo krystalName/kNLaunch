72d14fe5f720dfce020509f82309825ed02cede7
Leessang <992275294@qq.com>
2018-08-15 14:57:51 +0800

    Initial Commit

---
+++ KNLaunchPage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNLaunchPage
+//
+//  Created by 刘凡 on 2018/8/15.
+//  Copyright © 2018年 leesang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNLaunchPage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNLaunchPage
+//
+//  Created by 刘凡 on 2018/8/15.
+//  Copyright © 2018年 leesang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
